ecd7ff9c18743afb0f8062f827302c5a0254bde4
wuwenlong <773314621@qq.com>
2025-03-11 09:38:08 +0800

    fixbug

--- src/middlewares/subdomainMiddleware.ts
+++ src/middlewares/subdomainMiddleware.ts
@@ -7,17 +7,18 @@ function removeText(input: string, textToRemove: string): string {
     const regex = new RegExp(textToRemove, "g"); // 创建一个全局匹配的正则表达式
     return input.replace(regex, ""); // 替换掉指定的文字
   }
+  const getSubdomain = (host:string) => {
+    if (process.env.CROS_DOMAIN === host) return null
+    return removeText(host, '.'+process.env.CROS_DOMAIN);
+  };
 export const checkSystemInitialized = async (
   req: Request & { subdomain: string | null,subdomain_id:Types.ObjectId|undefined},
   res: Response,
   next: NextFunction
 ) => {
   const host = req.hostname; // 获取主机名
-  const getSubdomain = () => {
-    if (process.env.CROS_DOMAIN === host) return null
-    return removeText(host, '.'+process.env.CROS_DOMAIN);
-  };
-  const subdomain = getSubdomain()
+  if(host === undefined) return res.status(400).json({ code: 1, message: "host is undefined" });
+  const subdomain = getSubdomain(host)
   const site = await Site.findOne({ site_sub_url: subdomain });
   if (!site && subdomain !== null ) {
     return res.status(404).json({ code: 2, message: "站点不存在" });
